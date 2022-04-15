b810ea756ade5a9f3d21498c6f6fa1622b8126cb
Omolara5861 <debbiegterra@gmail.com>
2022-04-15 22:10:09 +0100

    pulling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LinksDataBrokerServiceToken, LinksPageModule } from 'links-lib';
 import { LocalLinksDataBrokerService } from './services/local-links-data-broker/local-links-data-broker.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, LinksPageModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, LinksPageModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   // provide the data-broker implementation
   {provide:LinksDataBrokerServiceToken , useClass:LocalLinksDataBrokerService}],
--- src/app/services/local-links-data-broker/local-links-data-broker.service.ts
+++ src/app/services/local-links-data-broker/local-links-data-broker.service.ts
@@ -27,7 +27,7 @@ export class LocalLinksDataBrokerService extends ImplLinksDataBroker{
         ui:{
           pages:{
             links:{
-              
+
             }
           }
         }
